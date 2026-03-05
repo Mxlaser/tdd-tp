5e1ade4048ce729e20e3a81fd1eab4bf94729b15
AdaamEL <91120983+AdaamEL@users.noreply.github.com>
2026-03-05 14:58:19 +0100

    [feat] (poker.py) logique pour detecter une paire

--- poker.py
+++ poker.py
@@ -1,3 +1,5 @@
+import collections
+
 class Card:
     def __init__(self, rank, suit):
         self.rank = rank
@@ -8,4 +10,13 @@ class Card:
 
 def evaluate_5_cards(cards):
     ranks = sorted([c.rank for c in cards], reverse=True)
+    counts = collections.Counter(ranks)
+    
+    counts_sorted = sorted(counts.items(), key=lambda x: (x[1], x[0]), reverse=True)
+    pattern = [x[1] for x in counts_sorted]
+    ranked_vals = [x[0] for x in counts_sorted]
+
+    if pattern == [2, 1, 1, 1]:
+        return (1, ranked_vals), "One pair"
+    
     return (0, ranks), "High card"
