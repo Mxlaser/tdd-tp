c65b434837a7843594069170661b9d01d555ee72
AdaamEL <91120983+AdaamEL@users.noreply.github.com>
2026-03-05 14:49:58 +0100

    [feat] (test_poker.py) création du premier test

---
+++ test_poker.py
@@ -0,0 +1,12 @@
+import unittest
+from poker import Card, evaluate_5_cards, best_hand
+
+class TestPoker(unittest.TestCase):
+    def test_high_card(self):
+        cards = [Card(14, 'Hearts'), Card(10, 'Spades'), Card(7, 'Clubs'), Card(4, 'Diamonds'), Card(2, 'Hearts')]
+        score, name = evaluate_5_cards(cards)
+        self.assertEqual(name, "High card")
+        self.assertEqual(score[0], 0)
+
+if __name__ == '__main__':
+    unittest.main()
